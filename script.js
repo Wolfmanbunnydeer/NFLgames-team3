9df6833ea4834112eae42d9e36a91dcc4f2e7623
Lougosi <83800145+Wolfmanbunnydeer@users.noreply.github.com>
2022-11-11 19:32:19 -0500

    Update script.js
    Signed-off-by: Lougosi <83800145+Wolfmanbunnydeer@users.noreply.github.com>

--- script.js
+++ script.js
@@ -515,14 +515,14 @@ function AddGame() {
   }
   document.getElementById("Game").innerHTML = text;
 }
-window.location.href = 'https://wolfmanbunnydeer.github.io/NFLgameteam/?'; //Will take you to Github.
-window.open('https://wolfmanbunnydeer.github.io/NFLgameteam/?'); //This will open Google in a new window.
+window.location.href = 'https://wolfmanbunnydeer.github.io/NFLgameteam/'; //Will take you to Github.
+window.open('https://wolfmanbunnydeer.github.io/NFLgameteam/'); //This will open Google in a new window.
 function newDoc() {
-  window.location.assign("https://wolfmanbunnydeer.github.io/NFLgameteam/?")
+  window.location.assign("https://wolfmanbunnydeer.github.io/NFLgameteam/")
 }   
   document.getElementById("Game").innerHTML = text;
-window.location.href = 'https://wolfmanbunnydeer.github.io/NFLgameteam/?'; //Will take you to Github.
-window.open('https://wolfmanbunnydeer.github.io/NFLgameteam/?'); //This will open Google in a new window.
+window.location.href = 'https://wolfmanbunnydeer.github.io/NFLgameteam/'; //Will take you to Github.
+window.open('https://wolfmanbunnydeer.github.io/NFLgameteam/'); //This will open Google in a new window.
 function newDoc() {
-  window.location.assign("https://wolfmanbunnydeer.github.io/NFLgameteam/?")
+  window.location.assign("https://wolfmanbunnydeer.github.io/NFLgameteam/")
 }      
